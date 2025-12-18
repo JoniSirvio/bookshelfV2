892df4d233860a18dc8639de9410be166e18687c
Joni Sirviö <t3sijo05@students.oamk.fi>
2025-12-18 17:31:53 +0200

    chore: revert CI configuration changes (metro, eas.json, workflow)

--- metro.config.js
+++
@@ -1,7 +0,0 @@
-const { getDefaultConfig } = require('@expo/metro-config');
-
-module.exports = (async () => {
-    const config = await getDefaultConfig(__dirname);
-    config.resolver.sourceExts.push('cjs');
-    return config;
-})();
